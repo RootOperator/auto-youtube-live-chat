c3a31fbbd47f867b823fc55e5aac7f2a69a4c8ea
RootOperator <20143055+RootOperator@users.noreply.github.com>
2022-07-20 17:08:51 +0200

    Create LiveChat.js

---
+++ LiveChat.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name     Live Chat
+// @version  1
+// @author   RootOperator
+// @match https://www.youtube.com/live_chat*
+// @match https://www.youtube.com/live_chat_replay*
+// ==/UserScript==
+
+function getElementByXpath(path) {
+  return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+}
+
+function change_chat() {
+  let chatbtn = getElementByXpath('//*[@id="label-text"]')
+  chatbtn.click()
+
+  let livechat = getElementByXpath('/html/body/yt-live-chat-app/div/yt-live-chat-renderer/iron-pages/div/yt-live-chat-header-renderer/div[1]/span[2]/yt-sort-filter-sub-menu-renderer/yt-dropdown-menu/tp-yt-paper-menu-button/tp-yt-iron-dropdown/div/div/tp-yt-paper-listbox/a[2]/tp-yt-paper-item/tp-yt-paper-item-body/div[1]')
+  livechat.click()
+}
+
+function check_chat() {
+   if (document.getElementById("chat")) {
+     if (document.getElementById('label-text').innerText == "Top chat") {
+       change_chat();
+     }
+   }
+}
+
+setInterval(check_chat, 1000);
