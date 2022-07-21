4bed5065415682d024a90d10c9c84067d31edb32
RootOperator <20143055+RootOperator@users.noreply.github.com>
2022-07-21 12:37:19 +0200

    Update LiveChat.js

--- LiveChat.js
+++ LiveChat.js
@@ -6,17 +6,14 @@
 // @match https://www.youtube.com/live_chat_replay*
 // ==/UserScript==
 
-function getElementByXpath(path) {
-  return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-}
-
-function check_chat() {
+setInterval(function() {
    if (document.getElementById("chat")) {
      if (document.getElementById('label-text').innerText == "Top chat") {
-         let livechat = getElementByXpath('/html/body/yt-live-chat-app/div/yt-live-chat-renderer/iron-pages/div/yt-live-chat-header-renderer/div[1]/span[2]/yt-sort-filter-sub-menu-renderer/yt-dropdown-menu/tp-yt-paper-menu-button/tp-yt-iron-dropdown/div/div/tp-yt-paper-listbox/a[2]/tp-yt-paper-item/tp-yt-paper-item-body/div[1]')
-         livechat.click()
+         document.evaluate('/html/body/yt-live-chat-app/div/yt-live-chat-renderer/iron-pages/div/yt-live-chat-header-renderer/div[1]/span[2]/yt-sort-filter-sub-menu-renderer/yt-dropdown-menu/tp-yt-paper-menu-button/tp-yt-iron-dropdown/div/div/tp-yt-paper-listbox/a[2]/tp-yt-paper-item/tp-yt-paper-item-body/div[1]',
+                           document,
+                           null,
+                           XPathResult.FIRST_ORDERED_NODE_TYPE,
+                           null).singleNodeValue.click();
      }
    }
-}
-
-setInterval(check_chat, 1000);
+}, 1000);
