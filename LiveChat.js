07e5fbd21a1474ac4a926ea7dc71caa404bbb880
RootOperator <20143055+RootOperator@users.noreply.github.com>
2022-07-21 12:34:54 +0200

    Update LiveChat.js

--- LiveChat.js
+++ LiveChat.js
@@ -10,18 +10,11 @@ function getElementByXpath(path) {
   return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 }
 
-function change_chat() {
-  let chatbtn = getElementByXpath('//*[@id="label-text"]')
-  chatbtn.click()
-
-  let livechat = getElementByXpath('/html/body/yt-live-chat-app/div/yt-live-chat-renderer/iron-pages/div/yt-live-chat-header-renderer/div[1]/span[2]/yt-sort-filter-sub-menu-renderer/yt-dropdown-menu/tp-yt-paper-menu-button/tp-yt-iron-dropdown/div/div/tp-yt-paper-listbox/a[2]/tp-yt-paper-item/tp-yt-paper-item-body/div[1]')
-  livechat.click()
-}
-
 function check_chat() {
    if (document.getElementById("chat")) {
      if (document.getElementById('label-text').innerText == "Top chat") {
-       change_chat();
+         let livechat = getElementByXpath('/html/body/yt-live-chat-app/div/yt-live-chat-renderer/iron-pages/div/yt-live-chat-header-renderer/div[1]/span[2]/yt-sort-filter-sub-menu-renderer/yt-dropdown-menu/tp-yt-paper-menu-button/tp-yt-iron-dropdown/div/div/tp-yt-paper-listbox/a[2]/tp-yt-paper-item/tp-yt-paper-item-body/div[1]')
+         livechat.click()
      }
    }
 }
